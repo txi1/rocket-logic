78bf3e4f7d709717fcbb4632dcae5518b3aba280
Taylor Xi <caetortaylor@gmail.com>
2021-01-30 14:30:22 -0500

    created Game class

--- game.py
+++ game.py
@@ -4,26 +4,48 @@ import pygame
 import sys
 from time import sleep
 
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.running = True
+        self.playing = False
+        self.UP, self.DOWN, self.START, self.BACK = False, False, False, False
+        #self.player = rocket()
+        self.windowX, self.windowY = 800, 500
+        self.display = pygame.Surface((self.windowX, self.windowY))
+        self.screen = pygame.display.set_mode([self.windowX, self.windowY])
 
-pygame.init
-windowX = 800
-windowY = 500
 
-screen = pygame.display.set_mode([windowX,windowY])
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    self.START = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.BACK = True
+                if event.key == pygame.K_UP:
+                    self.UP = True
+                if event.key == pygame.K_DOWN:
+                    self.DOWN = True
 
-def main():
-	running = True
-	player = rocket()
-	while running:
-		screen.fill((255,255,255))	
-		
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
+    def reset_keys(self):
+        self.UP, self.DOWN, self.START, self.BACK = False, False, False, False
 
-		player.draw()
-		pygame.display.update()
-		
+    def game_loop(self):
+        while self.playing:
+            self.check_events()
+            if self.START:
+                self.playing = False
+            self.screen.fill((255,255,255))		
+            #self.player.draw()
+            self.screen.blit(self.display, (0,0))
+            pygame.display.update()
+            self.reset_keys()
+
+
+
 class rocket():
 
 	def __init__(self):
@@ -43,4 +65,8 @@ class asteroid():
 
 
 
-main()
+start_game = Game()
+
+while start_game.running:
+    start_game.playing = True
+    start_game.game_loop()
--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 import pygame
 
-class game()
-  def __init__ (self)
+class game():
+  def __init__ (self):
     pygame.init()
     self.running, self.playing = True, False
     self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
@@ -14,3 +14,14 @@ class game()
 
       pygame.draw.rect(win, (255, 0, 0), (50, 50, 50, 50))
       pygame.display.update()
+
+class Menu():
+  def __init__(self, game):
+    self.game = game
+    self.mid_height, self.mid_width = self.game.WindowX/2, self.game.WindowY/2
+    self.run_display = True
+    self.cursor_rect = pygame.Rect(0, 0, 20, 20)
+    self.offset = -100
+
+  def draw_cursor():
+    self.game.
