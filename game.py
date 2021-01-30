e93fb5e4178940026f047e6a63dd5a357f216549
yourBoiGershy <egershte@gmail.com>
2021-01-30 14:49:57 -0500

    added asteroid and collision check

--- game.py
+++ game.py
@@ -8,12 +8,15 @@ from time import sleep
 pygame.init
 windowX = 800
 windowY = 500
+speed = 0.5
+
 
 screen = pygame.display.set_mode([windowX,windowY])
 
 def main():
 	running = True
 	player = rocket()
+	rock = asteroid()
 	while running:
 		screen.fill((255,255,255))	
 		
@@ -21,8 +24,35 @@ def main():
 			if event.type == pygame.QUIT:
 				running = False
 
+			keys = pygame.key.get_pressed()
+			if(keys[pygame.K_UP]):
+				player.moveUp = True
+			if(keys[pygame.K_DOWN]):
+				player.moveDown = True
+			if(keys[pygame.K_SPACE]):
+				rock.stopMoving = True
+
+			if(keys[pygame.K_UP]==False):
+				player.moveUp = False
+			if(keys[pygame.K_DOWN]==False):
+				player.moveDown = False
+			if(keys[pygame.K_SPACE]==False):
+				rock.stopMoving = False
+
+
+		if(player.x + player.w > rock.x and player.x < rock.x + rock.w and player.y + player.h > rock. y and player.y < rock.y + rock.h):
+			print("collision occured!")
+
+		player.update()
 		player.draw()
+		rock.update()
+		rock.draw()
+		
+
+		pygame.time.delay(8)
+
 		pygame.display.update()
+		
 		
 class rocket():
 
@@ -30,16 +60,40 @@ class rocket():
 
 		self.x = 20
 		self.y = 225
+		self.w = 50
+		self.h = 50
+		self.moveUp = False
+		self.moveDown = False
 
 	def draw(self):
 		
-		pygame.draw.rect(screen,(0,0,255), (self.x,self.y,50,50))
+		pygame.draw.rect(screen,(0,0,255), (self.x,self.y,self.w,self.h))
+
+	def update(self):
+		if self.moveUp:
+			self.y -= 4
+		if self.moveDown:
+			self.y += 4
 
 class asteroid():
 
 	def __init__(self):
 		
-		self.x = 800
+		self.x = windowX
+		self.y = 200
+		self.w = 800
+		self.h = 100
+		self.stopMoving = False
+
+	def draw(self):
+		
+		pygame.draw.rect(screen,(0,255,255),(self.x,self.y,self.w,self.h))
+
+	def update(self):
+		if self.stopMoving == False:
+			self.x -= speed
+
+
 
 
 
