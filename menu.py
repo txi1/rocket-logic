7c02e633753c3c6a44c24d763b40b5871c6bcde9
Taylor Xi <caetortaylor@gmail.com>
2021-01-30 13:43:28 -0500

    changed to class

--- menu.py
+++ menu.py
@@ -1,15 +1,16 @@
 import pygame
-pygame.init()
 
-win = pygame.display.set_mode((0,0), pygame.FULLSCREEN | pygame.RESIZABLE)
+class game()
+  def __init__ (self)
+    pygame.init()
+    self.running, self.playing = True, False
+    self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+    self.win = pygame.display.set_mode((0,0), pygame.FULLSCREEN | pygame.RESIZABLE)
+    pygame.display.set_caption("Rocket Logic: Not quite rocket science")
+    while self.running:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          self.running = False
 
-pygame.display.set_caption("Rocket Logic: Not quite rocket science")
-
-running = True
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-
-  pygame.draw.rect(win, (255, 0, 0), (50, 50, 50, 50))
-  pygame.display.update()
+      pygame.draw.rect(win, (255, 0, 0), (50, 50, 50, 50))
+      pygame.display.update()
