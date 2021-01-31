c88752b2e1e860f3ce59b5e10052237e88b4ce11
William Yin <william.yin00@gmail.com>
2021-01-30 20:04:55 -0800

    remade changes because eric doesnt know how to pull

--- equations.py
+++ equations.py
@@ -28,38 +28,38 @@ def complicate_equation(current_equation, complexity, true_expressions, false_ex
         return complicate_equation(new_equation,complexity-1,true_expressions,false_expressions,contingent_expressions)
 
 def replace_parts(to_change,changes,true_expressions,false_expressions,contingent_expressions):
-    for x in range(changes):
-        location_of_letters = find('T',to_change) + find('F',to_change) + find('p',to_change)
-        if len(location_of_letters):
-            a = random.choice(location_of_letters)
-            original_length = len(to_change)
-            if to_change[a] == 'T':
-                if random.randint(1,2) == 2:
-                    to_change = to_change[0:a] + '('+random.choice(true_expressions)+')' + to_change[a+1:]
-                else:
-                    to_change = to_change[0:a] + '¬('+random.choice(false_expressions)+')' + to_change[a+1:]
-                difference = len(to_change)-original_length
-                for item in location_of_letters:
-                    if item > a:
-                        item+=difference
-                location_of_letters.pop(location_of_letters.index(a))
-            elif to_change[a] == 'F':
-                if random.randint(1,2) == 2:
-                    to_change = to_change[0:a] + '('+random.choice(false_expressions)+')' + to_change[a+1:]
-                else:
-                    to_change = to_change[0:a] + '¬('+random.choice(true_expressions)+')' + to_change[a+1:]
-                difference = len(to_change)-original_length
-                for item in location_of_letters:
-                    if item > a:
-                        item+=difference
-                location_of_letters.pop(location_of_letters.index(a))
-            elif to_change[a] == 'p':
-                to_change = to_change[0:a] +'('+random.choice(contingent_expressions)+')' + to_change[a+1:]
-                difference = len(to_change)-original_length
-                for item in location_of_letters:
-                    if item > a:
-                        item+=difference
-                location_of_letters.pop(location_of_letters.index(a))
+
+    location_of_letters = find('T',to_change) + find('F',to_change) + find('p',to_change)
+    if len(location_of_letters):
+        a = random.choice(location_of_letters)
+        original_length = len(to_change)
+        if to_change[a] == 'T':
+            if random.randint(1,2) == 2:
+                to_change = to_change[0:a] + '('+random.choice(true_expressions)+')' + to_change[a+1:]
+            else:
+                to_change = to_change[0:a] + '¬('+random.choice(false_expressions)+')' + to_change[a+1:]
+            difference = len(to_change)-original_length
+            for item in location_of_letters:
+                if item > a:
+                    item+=difference
+            location_of_letters.pop(location_of_letters.index(a))
+        elif to_change[a] == 'F':
+            if random.randint(1,2) == 2:
+                to_change = to_change[0:a] + '('+random.choice(false_expressions)+')' + to_change[a+1:]
+            else:
+                to_change = to_change[0:a] + '¬('+random.choice(true_expressions)+')' + to_change[a+1:]
+            difference = len(to_change)-original_length
+            for item in location_of_letters:
+                if item > a:
+                    item+=difference
+            location_of_letters.pop(location_of_letters.index(a))
+        elif to_change[a] == 'p':
+            to_change = to_change[0:a] +'('+random.choice(contingent_expressions)+')' + to_change[a+1:]
+            difference = len(to_change)-original_length
+            for item in location_of_letters:
+                if item > a:
+                    item+=difference
+            location_of_letters.pop(location_of_letters.index(a))
     return to_change
 
 def convert(equation):
